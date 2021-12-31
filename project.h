66a1b0e04880159fc8784663a2c86fa5b00d146b
OliverAlejos <90672319+OliverAlejos@users.noreply.github.com>
2021-12-31 13:30:29 -0500

    Create project.h

---
+++ project.h
@@ -0,0 +1,34 @@
+/*
+ * project.h
+ */
+#ifndef PROJECT_H /*what are we gonan name this lol*/
+#define PROJECT_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/*
+ * Global Variable declarations, if needed.
+ */
+   int amount = 0;
+    char name;
+/*
+ * Data Structure declarations, if needed.
+ */
+typedef struct item{
+	int productNumber;
+	char productName[30];
+	int quantity;
+}
+item;
+/*
+ * Function declarations, if needed.
+ */
+void clrscr();
+void load_menu(void); 
+void additem (void);
+void removeitem(void);
+void viewproduct(void);
+
+#endif /* PROJECT_H */
